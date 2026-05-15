aa3f90d300258cfe79e6229cf3628d1f4cf621ed
sunmin416 <rhtjsals0106@gmail.com>
2026-05-15 11:54:30 +0900

    스웨거 오류 수정

---
+++ src/main/java/com/purecheck/config/SwaggerConfig.java
@@ -0,0 +1,22 @@
+package com.purecheck.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Value("${server.url:http://localhost:8080}")
+    private String serverUrl;
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .servers(List.of(new Server().url(serverUrl)));
+    }
+}
